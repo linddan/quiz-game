a8349922dc05c5a26de26b269344027ac91ce97f
linddan <daniel.lindros@gig.com>
2020-12-08 21:42:25 +0100

    Add proper stats view

--- src/views/GameSummary.vue
+++ src/views/GameSummary.vue
@@ -1,19 +1,42 @@
 <template>
-    <section>
-        <div class="container columns is-vcentered">
-            <div class="column is-8 is-offset-2">
-                <h1 class="is-size-1 has-text-centered">Game Summary</h1>
-                <p class="has-text-centered">{{ userAnswers }}</p>
-                <p class="has-text-centered">{{ statistics }}</p>
-            </div>
-        </div>
-        <button class="button is-large" @click="onClick">
-            <span class="icon is-medium">
-                <i class="fas fa-angle-double-right"></i>
-            </span>
-            <span>Play again</span>
-        </button>
-    </section>
+    <h1 class="is-size-1 has-text-centered">Game Summary</h1>
+    <p class="has-text-centered">Here's some information about your game!</p>
+
+    <div class="box mt-5">
+        <ul>
+            <li>
+                Number of correct answers:
+                <span>{{ noOfCorrectAnswers }}</span>
+            </li>
+            <li>
+                Number of incorrect answers:
+                <span>{{ noOfIncorrectAnswers }}</span>
+            </li>
+            <li>
+                Number of unanswered questions:
+                <span>{{ noOfUnansweredQuestions }}</span>
+            </li>
+            <li v-if="shouldDisplayStat(averageTimePerQuestion)">
+                Average time per question:
+                <span>{{ averageTimePerQuestion }} seconds</span>
+            </li>
+            <li v-if="shouldDisplayStat(quickestAnswer)">
+                Quickest answer:
+                <span>{{ quickestAnswer }} seconds</span>
+            </li>
+            <li v-if="shouldDisplayStat(slowestAnswer)">
+                Slowest answer:
+                <span>{{ slowestAnswer }} seconds</span>
+            </li>
+        </ul>
+    </div>
+
+    <button class="button is-large" @click="onClick">
+        <span class="icon is-medium">
+            <i class="fas fa-angle-double-right"></i>
+        </span>
+        <span>Play again</span>
+    </button>
 </template>
 
 <script>
@@ -22,16 +45,33 @@ import { router, GAME_MENU } from '@/router';
 import { getStatistics } from '@/utils/game';
 export default {
     setup() {
-        const { userAnswers, resetGame } = useGame();
+        const { userAnswers, resetGame, questions } = useGame();
         const onClick = () => {
             resetGame();
             router.push({ name: GAME_MENU });
         };
 
+        const shouldDisplayStat = (state) => state !== -1;
+
+        const {
+            noOfCorrectAnswers,
+            noOfIncorrectAnswers,
+            noOfUnansweredQuestions,
+            averageTimePerQuestion,
+            quickestAnswer,
+            slowestAnswer,
+        } = getStatistics(userAnswers.value, questions.value);
+
         return {
             userAnswers,
             onClick,
-            statistics: getStatistics(userAnswers.value),
+            shouldDisplayStat,
+            noOfCorrectAnswers,
+            noOfIncorrectAnswers,
+            noOfUnansweredQuestions,
+            averageTimePerQuestion,
+            quickestAnswer,
+            slowestAnswer,
         };
     },
 };
